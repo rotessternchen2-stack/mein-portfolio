d9b043d918232b4285034776c132e867aefdec91
Maria Wallberg <maria.wallberg@check24.de>
2025-03-30 17:14:58 +0200

    styling raus in app css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,49 +1,22 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logoMaria.svg";
 
 const Navbar = () => {
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > 10);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
-    <nav
-      className={`bg-violet-300 w-full fixed top-0 justify-between items-center transition-all duration-300 ${
-        scrolled ? "bg-white/80 backdrop-blur shadow-sm" : ""
-      }`}
-    >
-      <div className="flex items-center ">
-        {/* Links */}
-        <div className=" px-4 hidden md:flex space-x-6 text-gray-600">
+    <nav className="bg-violet-300 fixed top-0 w-screen">
+      <div className="flex text-gray-700">
+        <div className="">
           <Link to="/about" className="hover:text-black">Über mich</Link>
           <Link to="/projects" className="hover:text-black">Projekte</Link>
         </div>
-
-        {/* Logo in der Mitte */}
-        <div>
-          <Link to="/">
-            <img
-              src={logo}
-              alt="Logo Maria"
-              className="h-10 md:h-12 px-4"
-            />
-          </Link>
-        </div>
-
-        {/* Rechts */}
-        <div className="md:flex space-x-6 text-gray-600 px-4">
+        <Link to="/">
+          <img src={logo} alt="Logo Maria" className="h-10 md:h-12" />
+        </Link>
+        <div className="">
           <Link to="/contact" className="hover:text-black">Kontakt</Link>
           <Link to="/" className="hover:text-black">Startseite</Link>
         </div>
       </div>
-
     </nav>
   );
 };
