30c44d351bb4a232ac70da872640f3257f0675fb
Maria Wallberg <maria.wallberg@check24.de>
2025-04-03 18:29:23 +0200

    cleanup app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import About from './pages/About.tsx'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.tsx";
 import Home from "./pages/Home.tsx";
-import Projects from "./pages/Portfolio.tsx";
 import Contact from "./pages/Contact.tsx";
 import Footer from './components/Footer.tsx'
 import Portfolio from './pages/Portfolio.tsx';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
